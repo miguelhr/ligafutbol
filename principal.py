8d7799eea6c9ce695917c55e3490de38242a949e
miguelhr <miguel2hermanas@gmail>
2013-02-15 12:36:30 +0100

    lista equipos

---
+++ principal.py
@@ -0,0 +1,28 @@
+import urllib2
+from xml.dom.minidom import parse
+
+APIKEY = "3102fc908ca426bc06c8cb1c48dcedd4"
+
+url = urllib2.urlopen("http://www.resultados-futbol.com/scripts/api/api.php?key="+APIKEY+"&format=xml&req=tables&league=1")
+lista= url.read()
+equipos=open("equipos","w")
+equipos.write(lista)
+equipos.close()
+
+dom = parse('equipos')
+
+nodo_equipos = dom.firstChild
+
+equipos =[]
+for nodo_equipo in nodo_equipos.getElementsByTagName('table'):
+    equipo= nodo_equipo.getElementsByTagName('team')[0].firstChild.data
+    equipos.append(equipo)
+
+equipos2 =[]
+for i in equipos:
+    sin_saltos = i.replace('\n','')
+    equipos2.append(sin_saltos)
+
+for i in range(20):
+    print equipos2[i]
+
