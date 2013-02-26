839edd61caf98f43e2d25ffabaf40207326b6253
miguelhr <miguel2hermanas@gmail>
2013-02-26 17:14:57 +0100

    reducir codigo y cambiar modulo

--- cliga.py
+++ cliga.py
@@ -1,59 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 import cgi
-import urllib2
-from xml.dom.minidom import parse
+from lxml import etree
+import requests
 
 #objetos
 APIKEY = "3102fc908ca426bc06c8cb1c48dcedd4"
-raiz = "http://www.resultados-futbol.com/scripts/api/api.php?key="+APIKEY+"&format=xml"
 
+#Obtenemos valores
 print "Content-Type: text/html\n"
 form = cgi.FieldStorage()
 name = form.getvalue("clasi")
 
-url = urllib2.urlopen(""+raiz+"&req=tables&league=%s" % name)
-
-#Reemplazos de palabras
-lista= url.read()
-lista=lista.replace("Atlético","Atletico")
-lista=lista.replace("Málaga","Malaga")
-lista=lista.replace("Almería","Almeria")
-lista=lista.replace("Alcorcón","Alcorcon")
-lista=lista.replace("Córdoba","Cordoba")
-lista=lista.replace("Mirandés","Mirandes")
-lista=lista.replace("Hércules","Hercules")
-lista=lista.replace("Paços","Pazos")
-lista=lista.replace("Río","Rio")
-lista=lista.replace("Marítimo","Maritimo")
-lista=lista.replace("Guimarães","Guimaraes")
-lista=lista.replace("Académica","Academica")
-lista=lista.replace("Setúbal","Setubal")
-equipos=open("fichero","w")
-equipos.write(lista)
-equipos.close()
+#Obtenemos XML
+payload = {'league': '%s'%name, 'req': 'tables', 'format': 'xml', 'key': APIKEY}
+r = requests.get('http://www.resultados-futbol.com/scripts/api/api.php?', params=payload)
+url= r.text
 
 #Leyendo XML
-dom = parse('fichero')
-nodo_equipos = dom.firstChild
+resp_xml = etree.fromstring(url)
 
-equipos =[]
-puntos =[]
-ganados =[]
-for nodo_equipo in nodo_equipos.getElementsByTagName('table'):
-    equipo= nodo_equipo.getElementsByTagName('team')[0].firstChild.data.replace('\n','')
-    punto= nodo_equipo.getElementsByTagName('points')[0].firstChild.data.replace('\n','')
-    ganado= nodo_equipo.getElementsByTagName('wins')[0].firstChild.data.replace('\n','')
-    equipos.append(equipo)
-    puntos.append(punto)
-    ganados.append(ganado)
+equipos = resp_xml.xpath("/tables/table/team/text()")
+puntos = resp_xml.xpath("/tables/table/points/text()")
+ganados = resp_xml.xpath("/tables/table/wins/text()")
 
-    #Cuenta el número de equipos
+#Cuenta el número de equipos
 Numeroequipos = len(equipos)
 
-    #Mostrar en pantalla clasificación
+#Mostrar en pantalla clasificación
 print("<html><head><title>Clasificacion</title></head>")
 print "<body><h5>Pos.     Equipos       Puntos  Ganados</h5>"
 for i in range(Numeroequipos):
-    print "<p>%s  %s  %s  %s</p></body></html>" % (i+1,equipos[i],puntos[i],ganados[i])
+    print "<p>%s  %s  %s  %s</p></body></html>" % (i+1,equipos[i].encode('utf-8'),puntos[i],ganados[i])
 
--- rjornada.py
+++ rjornada.py
@@ -1,56 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 import cgi
-import urllib2
-from xml.dom.minidom import parse
+from lxml import etree
+import requests
 
 #objetos
 APIKEY = "3102fc908ca426bc06c8cb1c48dcedd4"
-raiz = "http://www.resultados-futbol.com/scripts/api/api.php?key="+APIKEY+"&format=xml"
 
+#Obtenemos valores
 print "Content-Type: text/html\n"
 form = cgi.FieldStorage()
 name = form.getvalue("liga")
 name2 = form.getvalue("jornada")
 
-
-url = urllib2.urlopen(""+raiz+"&req=matchs&league="+"%s"%name+"&round="+"%s" % name2)
-lista= url.read()
-lista=lista.replace("Atlético","Atletico")
-lista=lista.replace("Málaga","Malaga")
-lista=lista.replace("Almería","Almeria")
-lista=lista.replace("Alcorcón","Alcorcon")
-lista=lista.replace("Córdoba","Cordoba")
-lista=lista.replace("Mirandés","Mirandes")
-lista=lista.replace("Hércules","Hercules")
-lista=lista.replace("Paços","Pazos")
-lista=lista.replace("Río","Rio")
-lista=lista.replace("Marítimo","Maritimo")
-lista=lista.replace("Guimarães","Guimaraes")
-lista=lista.replace("Académica","Academica")
-lista=lista.replace("Setúbal","Setubal")
-result=open("fichero","w")
-result.write(lista)
-result.close()
-
+#Obtenemos XML
+payload = {'req': 'matchs', 'league': '%s' % name, 'format': 'xml', 'key': APIKEY, 'round': '%s' % name2}
+r = requests.get('http://www.resultados-futbol.com/scripts/api/api.php?', params=payload)
+url= r.text
 
 #Leyendo XML
-dom = parse('fichero')
-nodo_result = dom.firstChild
+resp_xml = etree.fromstring(url)
 
-locales =[]
-visitantes =[]
-gollocales =[]
-golvisitantes =[]
-for nodo_resul in nodo_result.getElementsByTagName('match'):
-    local= nodo_resul.getElementsByTagName('local')[0].firstChild.data.replace('\n','')
-    visitante= nodo_resul.getElementsByTagName('visitor')[0].firstChild.data.replace('\n','')
-    gollocal= nodo_resul.getElementsByTagName('local_goals')[0].firstChild.data.replace('\n','')
-    golvisitante= nodo_resul.getElementsByTagName('visitor_goals')[0].firstChild.data.replace('\n','')
-    locales.append(local)
-    visitantes.append(visitante)
-    gollocales.append(gollocal)
-    golvisitantes.append(golvisitante)
+locales = resp_xml.xpath("/matchs/match/local/text()")
+visitantes = resp_xml.xpath("/matchs/match/visitor/text()")
+gollocales = resp_xml.xpath("/matchs/match/local_goals/text()")
+golvisitantes = resp_xml.xpath("/matchs/match/visitor_goals/text()")
 
 #Cuenta el número de equipos
 Numeroequipos = len(locales)
@@ -59,7 +33,7 @@ Numeroequipos = len(locales)
 print("<html><head><title>Resultados</title></head>")
 print "<body><h5>Local                   visitante</h5>"
 for i in range(Numeroequipos):
-    print "<p>%s  \t%s-%s\t%s</p></body></html>" % (locales[i],gollocales[i],golvisitantes[i],visitantes[i])
+    print "<p>%s  \t%s-%s\t%s</p></body></html>" % (locales[i].encode('utf-8'),gollocales[i],golvisitantes[i],visitantes[i].encode('utf-8'))
 
 
 
