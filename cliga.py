d709062045eecf7109f88ab134f6e5083fe3d1e0
miguelhr <miguel2hermanas@gmail>
2013-02-27 09:01:50 +0100

    clasificacion informacion completa

--- cliga.py
+++ cliga.py
@@ -22,7 +22,13 @@ resp_xml = etree.fromstring(url)
 
 equipos = resp_xml.xpath("/tables/table/team/text()")
 puntos = resp_xml.xpath("/tables/table/points/text()")
+jugados = resp_xml.xpath("/tables/table/round/text()")
 ganados = resp_xml.xpath("/tables/table/wins/text()")
+empatados = resp_xml.xpath("/tables/table/draws/text()")
+perdidos = resp_xml.xpath("/tables/table/losses/text()")
+gfavor = resp_xml.xpath("/tables/table/gf/text()")
+gcontra = resp_xml.xpath("/tables/table/ga/text()")
+gaverage = resp_xml.xpath("/tables/table/avg/text()")
 
 #Cuenta el número de equipos
 Numeroequipos = len(equipos)
@@ -31,5 +37,5 @@ Numeroequipos = len(equipos)
 print("<html><head><title>Clasificacion</title></head>")
 print "<body><h5>Pos.     Equipos       Puntos  Ganados</h5>"
 for i in range(Numeroequipos):
-    print "<p>%s  %s  %s  %s</p></body></html>" % (i+1,equipos[i].encode('utf-8'),puntos[i],ganados[i])
+    print "<p>%s  %s  %s  %s  %s  %s  %s  %s  %s  %s</p></body></html>" % (i+1,equipos[i].encode('utf-8'),puntos[i],jugados[i],ganados[i],empatados[i],perdidos[i],gfavor[i],gcontra[i],gaverage[i])
 
